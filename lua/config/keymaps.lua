e033fd76509b08c58fbd91f5eddcde9b89b4a052
williampilger <pilger.will@gmail.com>
2025-08-19 15:34:04 -0300

    upld

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Ctrl + Backspace apaga palavra inteira no modo insert
+vim.keymap.set("i", "<C-BS>", "<C-w>", { noremap = true })
+vim.keymap.set("i", "<C-h>", "<C-w>", { noremap = true }) -- fallback p/ terminais que mandam <C-h>
+
